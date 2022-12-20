fa6283cc2f3e7e1706d664065454793006331416
Tamaz.Bagdavadze <Tamaz.Bagdavadze@SPLP0137>
2022-12-20 17:12:36 +0400

    test

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -12,12 +12,12 @@ print (replace_text)
 
 search_text = "${{cookiecutter.ProjectName}}"
 
-with open(r'test.yml', 'r') as file:
+with open(r'../../.github/workflows/build.yml', 'r') as file:
 
     data = file.read()
     data = data.replace(search_text, replace_text)
 
-with open(r'test.yml', 'w') as file:
+with open(r'../../.github/workflows/build.yml', 'w') as file:
     file.write(data)
 
-print("Text replaced" + replace_text + " _ _ " + search_text)
+print("Text replaced")
