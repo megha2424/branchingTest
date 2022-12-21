f8e9478073cee6dd4ad3a02d26b503742593c497
Tamaz.Bagdavadze <Tamaz.Bagdavadze@SPLP0137>
2022-12-21 14:19:32 +0400

    remove extra variables

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,14 +1,6 @@
-import json
 
-# replace_text =""
+replace_text = "{{cookiecutter.ProjectName}}" 
 
-# f = open('../../cookiecutter.json')
-
-# data = json.load(f)
-
-replace_text = "{{cookiecutter.ProjectName}}" #data["ProjectName"].encode('utf-8')
-
-#print (replace_text.decode('utf-8'))
 print (replace_text)
 
 search_text = "[[project_working_directory]]"
@@ -16,12 +8,8 @@ search_text = "[[project_working_directory]]"
 print (search_text)
 
 with open(r'../../.github/workflows/build.yml', 'r') as file:
-
     data = file.read()
-    print(data)
-    #data = data.replace(search_text, replace_text.decode('utf-8'))
     data = data.replace(search_text, replace_text)
-    print(data)
 
 with open(r'../../.github/workflows/build.yml', 'w') as file:
     file.write(data)
