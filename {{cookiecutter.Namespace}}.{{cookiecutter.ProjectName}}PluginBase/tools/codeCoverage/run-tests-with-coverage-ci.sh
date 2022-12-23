e5c89acc8ad12c0ef790d9faefc785d92c3b21b8
Tamaz.Bagdavadze <Tamaz.Bagdavadze@SPLP0137>
2022-12-23 15:00:02 +0400

    remove tests

--- {{cookiecutter.Namespace}}.{{cookiecutter.ProjectName}}PluginBase/tools/codeCoverage/run-tests-with-coverage-ci.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-dotnet test ../../ --results-directory $(pwd)/../../tmp/coverage/test-results --collect:"XPlat Code Coverage" --no-restore -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=json,opencover,cobertura
